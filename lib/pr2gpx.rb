9c31aadde7f9312541e85a15d378c9b335886bc1
David Tischler <david.tischler@gmx.at>
2012-10-06 14:24:57 +1200

    Lots of refactoring.

--- lib/pr2gpx.rb
+++ lib/pr2gpx.rb
@@ -1,21 +1,143 @@
 require 'nokogiri'
+require 'nokogiri/xml'
 require 'pr2gpx/parser'
+require 'optparse'
 
-parser = ReportParser.new
-		
-stations = Hash.new
-puts ARG[0]
-Dir
-	.glob(ARGV[1])
-	.map { |filename| File.open filename do |file| file.read() end }
-	.each do |content|
-		reports = parser.parse(content)
-		if reports
-			reports.each do |report|
-				stations[report.callsign] = Hash.new unless stations.has_key? report.callsign
-				stations[report.callsign][report.date] = report unless stations[report.callsign].has_key? report.date
+def parse_options argv
+	options = {}
+
+	def set_option options, name
+		lambda { |options, name, value| options[name] = value }.curry.(options, name)
+	end
+
+	OptionParser.new do |o|
+		options[:path] = nil
+		o.on '-p', '--path PATH',
+			'specify the path to be searched',
+			set_option(options, :path)
+
+		options[:recurse] = false
+		o.on '-r', '--recurse',
+			'enable recursive searching' do
+				options[:recurse] = true
 			end
+
+		options[:output] = nil
+		o.on '-o', '--output [PATH]',
+			'specify the path write to. If omitted, the output is sent to STDOUT',
+			set_option(options, :output)
+
+		options[:callsign] = nil
+		o.on '-c', '--callsign "[CALLSIGN[,CALLSIGN]]"', Array,
+			'processes only the stations with the given callsigns',
+			set_option(options, :callsign)
+
+		options[:limit] = nil
+		o.on '-l', '--limit [LIMIT]', Integer,
+			'limit the result to the LIMIT newest entrys for each station',
+			set_option(options, :limit)
+
+		options[:help] = false
+		o.on '-h', '--help',
+			'display this screen' do
+				options[:help] = true
+			end
+		
+		$verbose = false
+		o.on '-v', '--verbose',
+			'turn on verbose mode' do $verbose = true end
+
+		begin
+			o.parse! argv
+
+			if not options[:help]
+				mandatory = [:path]
+				missing = mandatory.select { |param| options[param].nil? }
+				if not missing.empty?
+					puts "Missing options: #{missing.join(', ')}"
+					puts
+					options[:help] = true
+				end
+			end
+		rescue OptionParser::InvalidOption, OptionParser::MissingArgument
+			puts $!.to_s
+			options[:help] = true
+		end
+
+		if options[:help]
+			puts o
+			return nil
 		end
 	end
 
-stations.each { |key, value| puts value.sort }
+	options
+end
+
+def load_data search_path, callsigns
+	reportParser = ReportParser.new
+	stations = Hash.new
+
+	Dir
+		.glob(search_path)
+		.each do |filename|
+			content = nil
+			File.open filename do |file|
+				content = file.read()
+			end
+			reports = reportParser.parse(content)
+			if reports
+				reports.each do |report|
+					if not callsigns or callsigns.include? report.callsign
+						stations[report.callsign] = Hash.new unless stations.has_key? report.callsign
+						stations[report.callsign][report.date] = report unless stations[report.callsign].has_key? report.date
+					end
+				end
+			end
+		end
+
+	stations
+end
+
+options = parse_options ARGV
+exit if not options
+
+search_path = "#{options[:path]}/#{options[:recurse] ? '**/' : ''}*.msg"
+$stderr.puts "Searching #{search_path}" if $verbose
+
+stations = load_data(search_path, options[:callsign])
+
+def add_waypoint xml, report, element_name
+	xml.send(element_name, lat: report.position.latitude, lon: report.position.longitude) do
+		xml.name report.comment
+		xml.time report.date
+	end
+end
+
+def build_gpx stations, limit
+	builder = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
+		xml.gpx(xmlns: 'http://www.topografix.com/GPX/1/1') do
+			stations.each do |callsign, reports|
+				xml.trk(name: callsign) do
+					xml.trkseg do
+						sorted_reports = reports.sort_by { |date, report| date }.reverse
+						sorted_reports = sorted_reports.take(limit) if limit
+						sorted_reports.each do |date, report|
+							add_waypoint xml, report, "trkpt"
+						end
+					end
+				end
+			end
+		end
+	end
+	builder.to_xml
+end
+
+gpx = build_gpx(stations, options[:limit])
+
+if options[:output] then
+	File.open options[:output], 'w:UTF-8' do |file|
+		file.write gpx
+	end
+else
+	puts gpx
+end
